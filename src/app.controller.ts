588d1bb789d0f081416ba54f70d2b1639f131f63
kimsun721 <kimsun050820@gmail.com>
2025-12-03 02:36:56 +0900

    chore: initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,10 @@
+import { Controller } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
+}
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ZodValidationPipe } from 'nestjs-zod';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { CustomExceptionFilter } from './common/filter/exception.filter';
+import { TransformInterceptor } from './common/interceptor/transform.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ZodValidationPipe());
+  app.enableShutdownHooks();
+  app.useGlobalFilters(new CustomExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  app.enableCors({
+    origin: `${process.env.FRONT_URL}`,
+    Credential: true,
+  });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('example')
+    .setDescription('example')
+    .setVersion('0.1')
+    .build();
+
+  await app.listen(process.env.SERVER_PORT ?? 3000);
+}
+bootstrap();
